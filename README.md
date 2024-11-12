bdea894a6cc254ff42c13c9ea6130536f3db3f2d
satsuki-thyme <satsuki.thyme@gmail.com>
2024-11-12 15:15:00 +0900

    やり直し

---
+++ README.md
@@ -0,0 +1,17 @@
+# maketable.js
+配列を与えるとテーブルを返す JavaScript のライブラリです。 
+
+## 目次
+- maketable.js: ライブラリ本体
+- maketable.min.js: ライブラリ本体（圧縮版）
+- sample.html: 使い方のサンプル
+- maketable.html: 使い方の応用編
+
+## 説明
+- 第 1 引数には tbody に入る値を配列で与えます。複数行は二次元配列で与えます
+- 第 2 引数には thead に入る値を配列で与えます。一次元配列である必要があります。省略が可能です
+- 第 3 引数にはテーブルの id を与えます。省略が可能です
+- 第 4 引数にはテーブルの class を与えます。省略が可能です
+
+## CDN
+https://cdn.jsdelivr.net/gh/satsuki-thyme/maketable.js@main/maketable.min.js
---
+++ maketable.js
@@ -0,0 +1,22 @@
+function maketable(tbodyArray, theadArray, clss, id) {
+  let insId = id ? ` id="${id}"` : ``
+  let insClss = clss ? ` class="${clss}"` : ``
+  let multiLines = (tbodyArray[0] instanceof Array || typeof tbodyArray[0] === `array`) ? true : false
+  let theadExist = theadArray ? true : false
+  let tbody = ``
+  let thead = ``
+  if (multiLines) {
+    let tr = ``
+    for (let tbItem of tbodyArray) {
+      tr += `<tr><td>${tbItem.join(`</td><td>`)}</td></tr>`
+    }
+    tbody = `<tbody>${tr}</tbody>`
+  }
+  else {
+    tbody = `<tbody><tr><td>${tbodyArray.join(`</td><td>`)}</td></tr></tbody>`
+  }
+  if (theadExist) {
+    thead = `<thead><tr><th>${theadArray.join(`</th><th>`)}</th></tr></thead>`
+  }
+  return `<table${insId + insClss}>${thead + tbody}</table>`
+}
